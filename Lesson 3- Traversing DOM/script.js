90ff074f239fdf0d4396c25ef9993bfa32f8a092
pavelFaraday <devstudio.corp@gmail.com>
2021-01-15 17:35:18 +0400

    Jquery | Lesson 3 - Traversing DOM

---
+++ Lesson 3- Traversing DOM/script.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+    // FIND method
+/*     
+    $('button').click(function (e) { 
+        $('#item-list').find('a').css('color', 'red');
+    }); 
+*/
+
+    // CHILDREN method
+/*
+    $('button').click(function (e) { 
+        $('#first-list').children('a').css('color', 'blue');
+    });
+*/
+
+    // CLOSEST method
+/*
+    $('button').click(function (e) { 
+        $('#inside-div').closest('li').css('background', 'blue');
+    });
+*/
+
+    // PARENT method
+/*    
+    $('button').click(function (e) { 
+        $('#inside-div').parent().css('border', '1px solid red');
+    });
+*/
+
+    // NEXT method
+/*
+    $('button').click(function (e) { 
+        $('#first-list').next().css('border', '1px solid red');
+    });
+*/
+
+    // SIBLINGS method
+/*
+    $('button').click(function (e) { 
+        $('#first-list').siblings().css('border', '1px solid red');
+    });
+*/
+
+    // FIRST method
+/*
+    $('button').click(function (e) { 
+        $('li').first().css('border', '1px solid red');
+    });
+*/
+
+    // LAST method
+/*
+    $('button').click(function (e) { 
+        $('li').last().css('border', '1px solid red');
+    });
+*/
+
+});
