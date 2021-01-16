fae91948f45e35e46f91b039f5384ef8daf1501d
pavelFaraday <devstudio.corp@gmail.com>
2021-01-16 16:32:06 +0400

    Jquery | Lesson 13 - Form Validation

---
+++ Lesson 13 - Form Validation/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('input:text').val();  // get value
+    $('input:text').val('world'); // set Value
+
+    $('input:password').val('123456'); // set Value
+    $('input:password').val(); // get value
+});
