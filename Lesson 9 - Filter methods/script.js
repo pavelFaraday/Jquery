e85cd9e9bd5424a8188e6b18b5b1b31133af037f
pavelFaraday <devstudio.corp@gmail.com>
2021-01-16 12:05:46 +0400

    Jquery | Lesson 9 - Filter methods

---
+++ Lesson 9 - Filter methods/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+
+    // eq(1) means: equals index #1
+    $('#btn1').click(function (e) { 
+        $('li').eq(1).toggleClass('border-red');
+    });  
+
+    // filter method
+    $('#btn2').click(function (e) { 
+        $('li').filter('.green').toggleClass('border-green');
+    });
+    
+    // has method - it is applyed on parent (ul)
+    $('#btn3').click(function (e) { 
+        $('.myUl').has('#yellow').toggleClass('border-yellow');
+    });
+
+    // not method
+    $('ul').not('.green').css('color', 'red');
+});
+
+
